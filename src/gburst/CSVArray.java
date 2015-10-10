2eb004499bf4a9f5114e76d1f0bfa335173b6780
James Barbour <jebarbou@cs.unc.edu>
2015-10-10 15:35:56 -0400

    Working processor, need getters, restructure class. Maybe singleton?

---
+++ src/gburst/CSVArray.java
@@ -0,0 +1,52 @@
+package gburst;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CSVArray {
+
+	private final Double[][] gData;
+	private final String[] locations;
+	private final String[][] taxonomy;
+
+	public CSVArray(File inFile) {
+		ArrayList<String[]> fileArray = new ArrayList<String[]>();
+		try {
+			Scanner in = new Scanner(inFile);
+			while (in.hasNextLine()) {
+				String temp = in.nextLine();
+				fileArray.add(temp.split(","));
+			}
+			in.close();
+		} catch (FileNotFoundException ex) {
+			throw new IllegalArgumentException("Invalid file");
+		}
+
+		//Create location array
+		String[] locTemp = new String[fileArray.get(0).length - 1];
+		for (int i = 1; i < fileArray.get(0).length; i++) {
+			locTemp[i - 1] = fileArray.get(0)[i];
+		}
+		locations = locTemp.clone();
+
+		//Create taxonomy array
+		taxonomy = new String[fileArray.size() - 1][];
+		for (int i = 1; i < fileArray.size(); i++) {
+			taxonomy[i - 1] = fileArray.get(i)[0].split(";");
+		}
+
+		//Create data array
+		gData = new Double[fileArray.size() - 1][fileArray.get(0).length - 1];
+		for (int i = 1; i < fileArray.size(); i++) {
+			for (int j = 1; j < fileArray.get(i).length; j++) {
+				gData[i - 1][j - 1] = Double.parseDouble(fileArray.get(i)[j]);
+			}
+		}
+		
+		System.out.println(Arrays.deepToString(taxonomy));
+		System.out.println(Arrays.deepToString(gData));
+		System.out.println(Arrays.toString(locations));
+	}
+}
---
+++ src/gburst/GDataProcessor.java
@@ -0,0 +1,48 @@
+package gburst;
+
+import java.io.File;
+
+public class GDataProcessor {
+	
+    public static final int ARGS_AMNT = 1;
+
+	/**
+	 * Sanity check for user input
+	 * @param args user input
+	 * @return true only if formatted as requested
+	 */
+	public static boolean validateArgs(String[] args) {
+		//Verify there is only one string, a file
+		if ((null == args) || (args.length != ARGS_AMNT)) { //SC_EVAL
+			throw new IllegalArgumentException("Invalid number of arguments");
+		} else if (null == args[0] || args[0].length() == 0) { //SC_EVAL
+			throw new IllegalArgumentException("Invalid input");
+		}
+		return true;
+	}
+	
+    /**
+     * @param args <file1>
+     */
+    public static void main(String[] args) {
+
+    	File inFile = new File("bin/test.csv");
+
+        //Validate input file exists, permissions are correct
+        try {    
+            if (!inFile.exists() || !inFile.canRead()) { //SC_EVAL
+                System.err.println("Error cannot locate or read input file: " +
+                        inFile.getName());
+                usage();
+                System.exit(0);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(
+                    "This line of code should never be reached.", ex);
+        }
+        
+        CSVArray test = new CSVArray(inFile);
+
+    }
+
+}
